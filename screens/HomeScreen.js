393bbb223f526dc548b10b1e42072db6095c03d4
christielise <jan.jime.serra@gmail.com>
2021-02-11 18:13:18 +0100

    add text

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,7 +4,7 @@ import { Button, Text, View } from 'react-native';
 export default function HomeScreen({ navigation }) {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home screen</Text>
+        <Text>Home screen (login)</Text>
         <Button
           title="Go to Details"
           onPress={() => navigation.navigate('Details')}
