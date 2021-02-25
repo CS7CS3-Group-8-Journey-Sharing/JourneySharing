5dc96cac6de2a9f7a5192c7e4cddffa502cca752
Jan J. Serra <jan.jime.serra@gmail.com>
2021-02-25 15:42:08 +0100

    cleanUp Hotfixes

--- screens/auth/SignUpScreen.js
+++ screens/auth/SignUpScreen.js
@@ -2,14 +2,17 @@ import * as React from "react";
 import { StyleSheet, View } from "react-native";
 import { Input } from "react-native-elements";
 import { Button } from "react-native-elements";
+import AuthContext from "../../context/AuthContext";
 
-export default function SignUpScreen({ route }) {
+export default function SignUpScreen() {
   const [firstname, setFirstname] = React.useState("");
   const [surname, setSurname] = React.useState("");
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [hidePassword, setHidePassword] = React.useState(true);
   const [passwordEye, setPasswordEye] = React.useState("eyeo");
+
+  const { signUp } = React.useContext(AuthContext);
 
   return (
     <View style={styles.container}>
@@ -36,13 +39,20 @@ export default function SignUpScreen({ route }) {
         label="Password"
         value={password}
         onChangeText={setPassword}
-        secureTextEntry = {hidePassword}
-        rightIcon={{ type: "ant-design", name: passwordEye, onPress: () => {setHidePassword(!hidePassword); setPasswordEye(hidePassword ? "eye" : "eyeo")} }} //https://oblador.github.io/react-native-vector-icons/
+        secureTextEntry={hidePassword}
+        rightIcon={{
+          type: "ant-design",
+          name: passwordEye,
+          onPress: () => {
+            setHidePassword(!hidePassword);
+            setPasswordEye(hidePassword ? "eye" : "eyeo");
+          },
+        }} //https://oblador.github.io/react-native-vector-icons/
       />
       <Button
         type="outline"
         title="Sign Up"
-        onPress={() => route.params.signUp({ firstname, surname, username, password })}
+        onPress={() => signUp({ firstname, surname, username, password })}
       />
     </View>
   );
