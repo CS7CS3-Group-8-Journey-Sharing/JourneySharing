7d4e316fb4b713d9cd485a60e6b8643c75745aa8
Jan J. Serra <jan.jime.serra@gmail.com>
2021-03-04 16:58:23 +0100

    add directions on FindJourneyScreen.js

--- screens/journey/FindJourneyScreen.js
+++ screens/journey/FindJourneyScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { View, StyleSheet, Dimensions, Platform } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
 
 export default function FindJourneyScreen() {
   const [region, setRegion] = useState({
@@ -10,9 +11,30 @@ export default function FindJourneyScreen() {
     longitudeDelta: 0.00421,
   });
 
+  const origin = { latitude: 53.347257, longitude: -6.2589555 };
+  const destination = { latitude: 53.3446581, longitude: -6.2563436 };
+  const GOOGLE_MAPS_APIKEY = "AIzaSyAAR-RYyfCvnHlgiLa1reZe7DpioIX04tM";
+
   return (
     <View style={styles.container}>
-      <MapView region={region} onRegionChange={setRegion} style={styles.map} />
+      <MapView
+        initialRegion={region}
+        onRegionChange={(newRegion) => setRegion(newRegion)}
+        style={styles.map}
+        //provider={PROVIDER_GOOGLE}
+      >
+        <MapView.Marker coordinate={origin} title="origin" />
+        <MapView.Marker coordinate={destination} title="destination" />
+        <MapViewDirections
+          origin={origin}
+          destination={destination}
+          mode="WALKING"
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor="darkgreen"
+          optimizeWaypoints={true}
+        />
+      </MapView>
     </View>
   );
 }
