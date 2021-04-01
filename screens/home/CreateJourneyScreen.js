3628f7af3ff3ce2023581a466d987cb10f03e0c7
d-egan <d-egan@users.noreply.github.com>
2021-04-01 16:49:56 +0100

    Create journey API test call

--- screens/home/CreateJourneyScreen.js
+++ screens/home/CreateJourneyScreen.js
@@ -13,6 +13,7 @@ import DropDownPicker from "react-native-dropdown-picker";
 import MapViewDirections from "react-native-maps-directions";
 import AuthContext from "../../context/AuthContext";
 import { Platform } from "react-native";
+import axios from "axios";
 
 export default function CreateJourneyScreen({ navigation }) {
   // get and use current location data
@@ -52,10 +53,54 @@ export default function CreateJourneyScreen({ navigation }) {
     }
   }
 
+  // Should probably tell people what they have to enter before creating
+  const readyToCreate = () => {
+    //TODO: Add more checks
+    return startMarker.set && endMarker.set
+  }
+
   function createJourney() {
-    // Check if all data is valid and both markers set
-    // or keep button disabled until everything is ready
-    // send it
+    console.log("Send it")
+    //TODO: validate data
+    axios
+      .post(
+        // https?
+        //TODO: localhost doesn't work on android, use 10.0.2.2 or proxy in emulator settings?
+        "http://10.0.2.2:8080/api/journeysharing/journey/createjourney",
+        {
+          maxParticipants: 99999,
+          modeOfTransport: transportMode.toUpperCase(),
+
+          //ownerId: userToken,
+          ownerId: "6065e0e6fdb39f04922f3d53",
+          participantIds: ["6065e1e3388f3868f0487e30", "6065e1fb388f3868f0487e31"],
+          //participantIds: [],
+
+          recurring: recurring,
+          startTime: startDate,
+          //TODO: Set actual end time
+          endTime: "3000-02-30T20:42:49.978Z",
+
+          startLocation: {
+            lat: startMarker.coordinate.latitude,
+            lng: startMarker.coordinate.longitude,
+            name: null
+          },
+          endLocation: {
+            lat: endMarker.coordinate.latitude,
+            lng: endMarker.coordinate.longitude,
+            name: null
+          },
+        }
+      )
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+      console.log("plz sens");
+
   }
 
   function setMarkersButton() {
@@ -183,9 +228,9 @@ export default function CreateJourneyScreen({ navigation }) {
 
         <Button
           type="outline"
-          disabled={true}
+          disabled={!readyToCreate()}
           title="Create Journey"
-          onPress={() => createJourney}
+          onPress={() => createJourney()}
         />
       </ScrollView>
     </View>
